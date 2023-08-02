94d0ca783b94d1ee76cf6992f241ee1cd35ff48a
Jeyell 26 <john_louise_bungay@dlsu.edu.ph>
2023-08-02 22:09:57 +0800

    elga.html finished, added DLSU Logo and changed carousel pics to actual

--- js/navjs.js
+++ js/navjs.js
@@ -1,57 +1,86 @@
-writeToHTML = `
-<nav class="navbar navbar-expand d-flex flex-column align-item-start" id="sidebar">
-<ul class="navbar-nav d-flex flex-column mt-5 w-100">
-    <li class="nav-item dropdown w-100">
-        <a href="#" class="nav-link dropdown-toggle text-light pl-4" id="homeDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-        Home
-        </a>
-        <ul class="dropdown-menu w-100" aria-labelledby="homeDropdown">
-            <li><a href="#" class="dropdown-item text-light pl-4 p-2">TITE </a></li>
-            <li><a href="#" class="dropdown-item text-light pl-4 p-2">Mission</a></li>
-            <li><a href="#" class="dropdown-item text-light pl-4 p-2">Program History</a></li>
-        </ul>
-    </li>
-    <li class="nav-item w-100">
-        <a href="#" class="nav-link text-light pl-4">Expected Lasallian Graduate Attributes (ELGA)</a>
-    </li>
-    <li class="nav-item w-100">
-        <a href="#" class="nav-link text-light pl-4">Program Educational Objectives (PEO)</a>
-    </li>
-    <li class="nav-item w-100">
-        <a href="#" class="nav-link text-light pl-4">Student Outcomes (SO)</a>
-    </li>
-    <li class="nav-item w-100">
-        <a href="#" class="nav-link text-light pl-4">Curriculum Map</a>
-    </li>
-    <li class="nav-item w-100">
-        <a href="#" class="nav-link text-light pl-4">Recognitions, Accreditations and Assessments</a>
-    </li>
-    <li class="nav-item dropdown w-100">
-        <a href="#" class="nav-link dropdown-toggle text-light pl-4" id="degreeDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-        Degree Programs
-        </a>
-        <ul class="dropdown-menu w-100" aria-labelledby="degreeDropdown">
-            <li><a href="#" class="dropdown-item text-light pl-4 p-2">Undergraduate Programs</a></li>
-            <li><a href="#" class="dropdown-item text-light pl-4 p-2">Graduate Programs</a></li>
-            <li><a href="#" class="dropdown-item text-light pl-4 p-2">Other Options and Specializations</a></li>
-        </ul>
-    </li>
-    <li class="nav-item w-100">
-        <a href="#" class="nav-link text-light pl-4">Enrollment and Graduation Statistics</a>
-    </li>
-    <li class="nav-item w-100">
-        <a href="#" class="nav-link text-light pl-4">Department Organizational Structure(PEO)</a>
-    </li>
-    <li class="nav-item w-100">
-        <a href="#" class="nav-link text-light pl-4">Faculty Profile</a>
-    </li>
-    <li class="nav-item w-100">
-        <a href="#" class="nav-link text-light pl-4">Laboratory and Facilities</a>
-    </li>
-</ul>
-</nav>
+writeToHTML =`
+<nav class="navbar" id="topbar">
+        <div class="container-fluid">
+            <button class="btn" id="menu-btn"> 
+                <img src="../res/navbar_burger.svg" width="30" height="30">
+            </button>
+            <div class="d-flex justify-content-center w-100">
+                <table id = "header">
+                <tr>
+                    <td rowspan="2"><img src="../res/dlsu_logo.png" width="50" height="50"></td>
+                    <td>De La Salle University </td>
+                </tr>
+                <tr>
+                    <td>Electronics and Computer Engineering</td>
+                </tr>
+                </table>
+                
+            </div>
+        </div>
+    </nav>
+
+    <nav class="navbar_sidebar navbar-expand d-flex flex-column align-item-start" id="sidebar">
+
+    <ul class="navbar-nav d-flex flex-column w-100">
+        <li class="nav-item dropdown w-100">
+            <a href="#" class="nav-link dropdown-toggle text-light pl-4" id="homeDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Home
+            </a>
+            <ul class="dropdown-menu w-100" aria-labelledby="homeDropdown">
+                <li><a href="index.html#vision" class="dropdown-item text-light pl-4 p-2">Vision </a></li>
+                <li><a href="#mission" class="dropdown-item text-light pl-4 p-2">Mission</a></li>
+                <li><a href="#progHisto" class="dropdown-item text-light pl-4 p-2">Program History</a></li>
+            </ul>
+        </li>
+        <li class="nav-item w-100">
+            <a href="elga.html" class="nav-link text-light pl-4">Expected Lasallian Graduate Attributes (ELGA)</a>
+        </li>
+        <li class="nav-item w-100">
+            <a href="peo.html" class="nav-link text-light pl-4">Program Educational Objectives (PEO)</a>
+        </li>
+        <li class="nav-item w-100">
+            <a href="#" class="nav-link text-light pl-4">Student Outcomes (SO)</a>
+        </li>
+        <li class="nav-item w-100">
+            <a href="#" class="nav-link text-light pl-4">Curriculum Map</a>
+        </li>
+        <li class="nav-item w-100">
+            <a href="#" class="nav-link text-light pl-4">Recognitions, Accreditations and Assessments</a>
+        </li>
+        <li class="nav-item dropdown w-100">
+            <a href="#" class="nav-link dropdown-toggle text-light pl-4" id="degreeDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Degree Programs
+            </a>
+            <ul class="dropdown-menu w-100" aria-labelledby="degreeDropdown">
+                <li><a href="#" class="dropdown-item text-light pl-4 p-2">Undergraduate Programs</a></li>
+                <li><a href="#" class="dropdown-item text-light pl-4 p-2">Graduate Programs</a></li>
+                <li><a href="#" class="dropdown-item text-light pl-4 p-2">Other Options and Specializations</a></li>
+            </ul>
+        </li>
+        <li class="nav-item w-100">
+            <a href="#" class="nav-link text-light pl-4">Enrollment and Graduation Statistics</a>
+        </li>
+        <li class="nav-item w-100">
+            <a href="#" class="nav-link text-light pl-4">Department Organizational Structure(PEO)</a>
+        </li>
+        <li class="nav-item w-100">
+            <a href="#" class="nav-link text-light pl-4">Faculty Profile</a>
+        </li>
+        <li class="nav-item w-100">
+            <a href="#" class="nav-link text-light pl-4">Laboratory and Facilities</a>
+        </li>
+    </ul>
+    </nav>
 `
+
 document.getElementById("movingSideBar").innerHTML = writeToHTML;
 
-
-
+window.onload = function () {
+    var menu_btn = document.querySelector("#menu-btn")
+        var sidebar = document.querySelector("#sidebar")
+        // var container = document.querySelector(".my-container")
+        menu_btn.addEventListener("click", () => {
+            sidebar.classList.toggle("active-nav")
+            // container.classList.toggle("active-cont")
+        })
+}
