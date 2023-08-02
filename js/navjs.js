e69a3fb41b3fe6ad2bdfa0078f04b7fd67b6ab4d
Jeyell 26 <john_louise_bungay@dlsu.edu.ph>
2023-08-02 19:27:07 +0800

    Finished Carousel

--- js/navjs.js
+++ js/navjs.js
@@ -52,14 +52,6 @@ writeToHTML = `
 </nav>
 `
 document.getElementById("movingSideBar").innerHTML = writeToHTML;
-{/* <script> */}
-        var menu_btn = document.querySelector("#menu-btn")
-        var sidebar = document.querySelector("#sidebar")
-        // var container = document.querySelector(".my-container")
-        menu_btn.addEventListener("click", () => {
-            sidebar.classList.toggle("active-nav")
-            // container.classList.toggle("active-cont")
-        })
-    {/* </script> */}
+
 
 
